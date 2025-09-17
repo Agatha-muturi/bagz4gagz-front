94ef476ea58972d26fea91b6d9c2799763783960
Agatha-muturi <muturigatha@gmail.com>
2025-09-17 14:56:07 +0300

    created a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import{Link} from 'react-router-dom';
+import { useCart } from '../context/cartcontext';
+
+
+export default function Navbar(){
+    
+     const { items } = useCart();
+     const count = items.reduce((s, it) => s + it.qty, 0);
+    
+     return(
+        <div>
+            <div>
+                <Link to='/' className="brand">BAGS4GAGZ</Link>
+            </div>
+            <nav>
+                <Link to='cart'>Cart</Link>
+                <Link to='products'>Products</Link>
+                <Link to='Checkout'>Chekout</Link>
+            </nav>
+        </div>
+    )
+}
