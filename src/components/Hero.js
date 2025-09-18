4d006b600ede275c773433db9f001346d546cd6c
Agatha-muturi <muturigatha@gmail.com>
2025-09-18 10:39:28 +0300

    created the hero page

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import {Link} from React;
+
+export default function Hero(){
+    return(
+        <section>
+            <div>
+                <h1>BAGZ4GAGZ</h1>
+                <p>Style that speaks- dresses and bags curated for you.</p>
+                <Link to='/products'>shop the Collection</Link>
+            </div>
+        </section>
+    )
+}
